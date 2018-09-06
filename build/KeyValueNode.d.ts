747719ba1931f18108699babe93582d9d98e42bc
Michael Price <mikepricefilm@gmail.com>
2018-09-06 13:28:10 -0600

    Builds v0.0.3

--- build/KeyValueNode.d.ts
+++ build/KeyValueNode.d.ts
@@ -1,10 +1,15 @@
 import { BaseKeyNode } from 'key-node';
+declare const VALUE: unique symbol;
+declare const DOC: unique symbol;
 export declare class BaseKeyValueNode<Tself extends BaseKeyValueNode = any> extends BaseKeyNode<Tself> {
-    readonly VALUE: any;
-    readonly DOC: object | any[];
+    private readonly [VALUE];
+    private readonly [DOC];
     constructor(key: string, parent: Tself);
     constructor(key: string, parent: Map<string, Tself>, rootDoc: object | any[]);
     readonly key: string;
+    readonly value: any;
+    readonly doc: object | any[];
 }
 export declare class KeyValueNode extends BaseKeyValueNode<KeyValueNode> {
 }
+export {};
